efe050ec3a43caadd851c7fa11db2a57f2436258
Hannah Bush <heb.rutgers@gmail.com>
2022-09-02 17:34:54 -0400

    intial commit!

---
+++ src/main/java/com/hannah/practiceb/PracticebApplication.java
@@ -0,0 +1,27 @@
+package com.hannah.practiceb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticebApplication {
+	private static final Logger logger = LoggerFactory.getLogger(PracticebApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticebApplication.class, args);
+		logger.info("\n" +
+				"---------------------------------------------------------------------------------------\n" +
+				"   ########  #######   ######    #######   ##     ##   ##      ######## ####### #######   \n" +
+				"   ##       ##      ## ##    ##  ##        ##     ##   ##         ##    ##      ##        \n" +
+				"   ##       ##      ## ##     ## ##        ##     ##   ##         ##    ##      ##        \n" +
+				"   ##       ##      ## ##     ## #####     #########   ##         ##    ######  #####     \n" +
+				"   ##       ##      ## ##     ## ##               ##   ##         ##    ##      ##        \n" +
+				"   ##       ##      ## ##    ##  ##               ##   ##         ##    ##      ##        \n" +
+				"   ########  #######   ######    #######          ##   ####### ######## ##      #######   \n" +
+				"---------------------------------------------------------------------------------------\n"
+		);
+		logger.info("Practice Backend Application is now running! Press the Red Stop Button in Eclipse to shutdown or CTRL+C in other IDEs.");
+	}
+}
---
+++ src/test/java/com/hannah/practiceb/PracticebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hannah.practiceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
