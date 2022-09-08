2b703badbe54f2a00b2799d5730d6426559e3642
Hannah Bush <heb.rutgers@gmail.com>
2022-09-08 18:43:54 -0400

    jwt config added

---
+++ src/main/java/com/hannah/practiceb/auth/JwtConfig.java
@@ -0,0 +1,51 @@
+package com.hannah.practiceb.auth;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.PostConstruct;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+
+public class JwtConfig {
+    //value allows you to use SpEL, but also get properties from the application.properties/yml
+    @Value("${jwt.secret}")
+    private String salt;
+
+    //example of using value w SpEL (spring expression language)
+    //to calculate the number of milliseconds in a day.
+    //Spring will then set this value for us
+    @Value("#{24*60*60*1000}")
+    private int expiration;
+
+    //this algorithm requires a 256 character (2048 bit) key
+    private static final SignatureAlgorithm sigAlg = SignatureAlgorithm.HS256;
+    private Key signingKey;
+
+    private String issuer = "hannah";
+
+    //this annotation means that the method should be
+    //executed after dependency injection is complete
+    @PostConstruct
+    public void createKey() {
+        byte[] saltyBytes = DatatypeConverter.parseBase64Binary(salt);
+        signingKey = new SecretKeySpec(saltyBytes, sigAlg.getJcaName());
+    }
+
+    public int getExpiration() {
+        return this.expiration;
+    }
+
+    public static SignatureAlgorithm getSigAlg() {
+        return sigAlg;
+    }
+
+    public Key getSigningKey() {
+        return signingKey;
+    }
+
+    public String getIssuer() {
+        return issuer;
+    }
+}
