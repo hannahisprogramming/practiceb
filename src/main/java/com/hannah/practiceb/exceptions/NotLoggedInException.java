88a733c0411c77b5dd0038d6890bee28c0f00e7f
Hannah Bush <heb.rutgers@gmail.com>
2022-09-15 21:39:04 -0400

    added rest exception handler and not logged in exception

---
+++ src/main/java/com/hannah/practiceb/exceptions/NotLoggedInException.java
@@ -0,0 +1,31 @@
+package com.hannah.practiceb.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/*
+ * Exception to be thrown by the AuthAspect
+ * Will be handled by a Spring Exception Handler to return a 401
+ */
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Not logged in!")
+public class NotLoggedInException extends RuntimeException {
+
+    public NotLoggedInException() {
+    }
+
+    public NotLoggedInException(String message) {
+        super(message);
+    }
+
+    public NotLoggedInException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotLoggedInException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotLoggedInException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
